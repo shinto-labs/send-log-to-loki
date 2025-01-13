3c5205d48463d68963e106a04b2248db50342a53
Aron Hemmes <aron@shintolabs.nl>
2025-01-13 11:37:17 +0100

    Update action to js

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # Send logs to Loki
+
+This GitHub Action sends logs to a Loki server. It can record the start time of a job and send log messages with various details about the workflow run.
+
+## Inputs
+
+- `measurement`: Controls the measurement type; "start" to record the start time of the job, "finish" or undefined to send the log message to Loki.
+- `status`: Controls the status of the log message; "success" or "failure".
+- `loki_address`: Loki server address required to send log.
+- `loki_username`: Loki username required to send log.
+- `loki_password`: Loki password required to send log.
+- `additional_labels`: Additional labels to be added to the log in logfmt format.
+
+## Default Labels
+
+The following default labels are provided in the log message:
+
+- `workflow`: The name of the workflow.
+- `repository_owner`: The owner of the repository.
+- `repository_name`: The name of the repository.
+- `status`: The status of the log message (success or failure).
+- `run_number`: The run number of the workflow.
+- `actor`: The user who triggered the workflow.
+- `url`: The URL to the workflow run.
+- `ref`: The git reference (branch or tag).
+- `duration`: The duration of the job in seconds (if the start time was recorded).
+
+## Example Workflow
+
+```yaml
+name: CI
+
+on: [push, pull_request]
+
+jobs:
+  send-logs:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+
+      - name: Start time
+        uses: shinto-labs/send-log-to-loki@v1.0.0
+        with:
+          measurement: start
+
+      - name: Run some steps
+        run: echo "Running some steps..."
+
+      - name: Send Logs to Loki
+        uses: shinto-labs/send-log-to-loki@v1.0.0
+        with:
+          loki_address: ${{ secrets.LOKI_ADDRESS }}
+          loki_username: ${{ secrets.LOKI_USERNAME }}
+          loki_password: ${{ secrets.LOKI_PASSWORD }}
+          status: success
+          additional_labels: "example_label=example_value"
+```
--- index.js
+++ index.js
@@ -1,5 +1,121 @@
-const { execSync } = require('child_process');
-const path = require('path');
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as fs from "fs";
+import * as path from "path";
 
-const scriptPath = path.join(__dirname, 'send_to_loki.sh');
-execSync(`bash ${scriptPath}`, { stdio: 'inherit' });
+const timeFilePath = path.join(
+  process.env.GITHUB_WORKSPACE || "",
+  "action_time.txt"
+);
+
+async function writeStartTime() {
+  const startTime = Date.now().toString();
+  core.info(`Writing start time: ${startTime}`);
+  fs.writeFileSync(timeFilePath, startTime);
+}
+
+async function sendLog(
+  loki_address,
+  loki_username,
+  loki_password,
+  status,
+  additional_labels
+) {
+  let log_message = `workflow=${github.context.workflow}`;
+  log_message += ` repository_owner=${github.context.repo.owner}`;
+  log_message += ` repository_name=${github.context.repo.repo}`;
+  log_message += ` status=${status}`;
+  log_message += ` run_number=${github.context.runNumber}`;
+  log_message += ` actor=${github.context.actor}`;
+  log_message += ` url=${github.context.payload.repository.html_url}/actions/runs/${github.context.runId}`;
+  log_message += ` ref=${github.context.ref}`;
+
+  if (fs.existsSync(timeFilePath)) {
+    const startTime = parseInt(fs.readFileSync(timeFilePath, "utf8"), 10);
+    const endTime = Date.now();
+    const duration = Math.round((endTime - startTime) / 1000);
+    core.info(`Calculated duration from start time: ${duration}`);
+    log_message += ` duration=${duration}`;
+  } else {
+    core.warning(
+      `
+      No start time found
+      add a step with the following code at the beginning of your workflow to calculate the duration:
+      - name: Start time
+        uses: shinto-labs/send-log-to-loki@v1.0.0
+        with:
+          measurement: start
+      `
+    );
+  }
+
+  log_message += additional_labels;
+
+  const log_entry = {
+    streams: [
+      {
+        stream: {
+          job: "github-actions",
+          level: status == "success" ? "info" : "error",
+        },
+        values: [[`${Date.now()}000000`, log_message]],
+      },
+    ],
+  };
+
+  core.info(`Sending log:\n${JSON.stringify(log_entry)}`);
+
+  try {
+    const response = await fetch(`${loki_address}/loki/api/v1/push`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${Buffer.from(
+          `${loki_username}:${loki_password}`
+        ).toString("base64")}`,
+      },
+      body: JSON.stringify(log_entry),
+    });
+    if (!response.ok) {
+      core.setFailed(`Failed to send log: ${response.statusText}`);
+    }
+  } catch (error) {
+    core.setFailed(`Failed to send log: ${error}`);
+  }
+}
+
+async function run() {
+  const measurement = core.getInput("measurement");
+  if (measurement == "start") {
+    await writeStartTime();
+  } else {
+    const loki_address = core.getInput("loki_address");
+    const loki_username = core.getInput("loki_username");
+    const loki_password = core.getInput("loki_password");
+    const status = core.getInput("status");
+
+    for (const [key, value] of Object.entries({
+      loki_address,
+      loki_username,
+      loki_password,
+      status,
+    })) {
+      if (!value) {
+        core.setFailed(`Missing required input: ${key}`);
+      }
+    }
+
+    let additional_labels = core.getInput("additional_labels");
+    additional_labels = additional_labels ? " " + additional_labels : "";
+
+    await sendLog(
+      loki_address,
+      loki_username,
+      loki_password,
+      status,
+      additional_labels
+    );
+  }
+}
+
+run();
